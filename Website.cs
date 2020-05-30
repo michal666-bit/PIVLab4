e381fd40a61919c331eeda5e744bc282ce18816e
michal666-bit <gajewski.michal88@gmail.com>
2020-05-30 12:21:37 +0200

    PIVLab4

---
+++ Website.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+
+namespace lab4p4
+{
+    class Website
+    {
+        public Website(string domain, string path, Method method = Method.GET)
+        {
+            _restClient = new RestClient(domain);
+            _restRequest = new RestRequest(path, method);
+        }
+        private RestClient _restClient;
+        private RestRequest _restRequest;
+
+        public async Task<string> Download()
+        {
+            var response = await _restClient.ExecuteAsync(_restRequest);
+            return response.Content;
+        }
+    }
+}
